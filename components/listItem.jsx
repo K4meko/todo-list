a288443b66c8c71eb8e386c9163b99ebcd83fb71
K4meko <pavlueliss@gmail.com>
2024-03-06 23:08:45 +0100

    added icons

--- components/listItem.jsx
+++ components/listItem.jsx
@@ -1,9 +1,15 @@
 import {View, Text, StyleSheet} from "react-native";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function ListItem() {
   return (
-    <View className="bg-red-300 rounded-lg h-10 w-11/12 justify-center">
-      <Text className="p-2">Do homework</Text>
+    <View className="bg-red-300 rounded-lg h-10 w-11/12  flex-row items-center justify-between p-2">
+      <Text className=" text-lg">Do homework</Text>
+      <MaterialCommunityIcons
+        name="checkbox-blank-circle-outline"
+        size={18}
+        color="black"
+      />
     </View>
   );
 }
