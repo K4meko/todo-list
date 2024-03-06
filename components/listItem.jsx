0517638a05f517e3b1480b2ff429dfe422f020b4
K4meko <pavlueliss@gmail.com>
2024-03-06 22:48:58 +0100

    Add existing project files to Git

---
+++ components/listItem.jsx
@@ -0,0 +1,9 @@
+import {View, Text, StyleSheet} from "react-native";
+
+export default function ListItem() {
+  return (
+    <View className="bg-red-300 rounded-lg h-10 w-11/12 justify-center">
+      <Text className="p-2">Do homework</Text>
+    </View>
+  );
+}
